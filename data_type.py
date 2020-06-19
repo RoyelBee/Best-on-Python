bdc9a27b2ec8e5275b72b9f4b4eb05421bd6248e
Rejaul Islam Royel <30933503+RoyelBee@users.noreply.github.com>
2020-06-19 14:29:00 +0600

    Data Type

---
+++ data_type.py
@@ -0,0 +1,73 @@
+# # Basic Data Types
+# Number(Integer, Floating), Sequence(String, List, Tuple)
+
+# # # Number -> Integer ----------------------------------
+#
+# production_cost = 2000
+# distribution_cost = 500
+# tax = 100.52342353
+# overall_sales = 5000
+#
+# print('Production Cost = ', production_cost)
+# print('Production Cost Data Type = ', type(production_cost))
+#
+#
+# # # Number -> Floating --------------------------------
+# print('Tax = ', tax)
+# print('Tak Data Type = ', type(tax))
+# # Limiting floating point number
+# print('Two decimal point = ', round(tax, 2))
+# print('Two decimal point = ', "{:.3f}".format(tax))
+
+# # Sequence -> String -------------------------------
+# first_name = "Rejaul Islam"
+# last_name = 'Royel'
+#
+# print('First Name = ', first_name)
+# print(type(first_name))
+# print('Last Name = ', last_name)
+# print(type(last_name))
+#
+# # Join String ---------------------------------------
+# full_name = first_name + last_name
+# print('Full Name = ', full_name)
+#
+# # String Concatenation
+# full_name = first_name + ' ' + last_name
+# print('Full Name = ', full_name)
+# # print('Full Name = ', first_name + ' ' + last_name)
+
+
+# # Sequence -> List -------------------------------
+
+num_list = [1, 2, 35, 7, 4, 6, 4, 6, 64, 3, 2]
+name_list = ['Sagir', 'Kanon', 'BM. Ashik', "Royel", 'Rabby']
+
+num_list = num_list[4
+,100]
+print(num_list)
+# print(num_list)
+# print(name_list)
+#
+# print('Num_list type', type(num_list))
+# print('name_list type', type(name_list))
+
+
+# print(num_list[0])
+# print(num_list[0:4])
+# print(num_list[:2])
+# print(num_list[2:5])
+
+# print(name_list[0])
+# print(name_list[0:4])
+# print(name_list[:2])
+# print(name_list[2:])
+
+# # Arithmetic operation in list
+# first = num_list[0]
+# second = num_list[1]
+# third = first + second
+# print(third)
+
+
+# # Sequence => Tuple---------------------------------
