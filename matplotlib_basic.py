8b8fac340c54f09a4b0aa7aa906f482866a8d055
Royel <royelsoft@gmail.com>
2020-06-28 16:14:53 +0600

    Update pandas, matplotlib_basic.py

--- matplotlib_basic.py
+++ matplotlib_basic.py
@@ -1,11 +1,9 @@
 from matplotlib import pyplot as plt
-import numpy as np
-import math  # needed for definition of pi
 
 # x = ['A', 'B', 'C', 'D', 'E']
 # y = [24, 40, 30, 50, 45]
 #
-# plt.plot(x, y, 'go..')
+# plt.plot(x, y, color='red')
 #
 # plt.xlabel("Name")
 # plt.ylabel("Marks")
@@ -15,17 +13,17 @@ import math  # needed for definition of pi
 # # For multiple lines ---------------------
 x = ['A', 'B', 'C', 'D', 'E']
 y = [24, 40, 30, 50, 45]
-y1 = [10, 20, 28, 14, 35]
+z = [10, 20, 28, 14, 35]
 
-colors = 'red'
+
 fig, ax = plt.subplots()
 line1 = ax.plot(x, y, color='red')
-line2 = ax.plot(x, y1, color='blue')
+line2 = ax.plot(x, z, color='blue')
 
 ax.legend(labels=('Math', 'English'), loc='best')  # legend placed at lower right
 plt.xlabel("Name",  color='black', fontsize=14, fontweight='bold')
 plt.ylabel("Marks", color='black', fontsize=14, fontweight='bold')
 plt.title('Line chart', fontweight='bold', color='#3e0a75',  fontsize=18)
-plt.show()
-# plt.savefig('linechart.png')
-
+# plt.show()
+# plt.tight_layout()
+plt.savefig('linechart.png')
--- pandas_practice.py
+++ pandas_practice.py
@@ -1,22 +1,29 @@
 import pandas as pd
 import pyodbc as db
 
-# Create Database connection---------------------------------
+# # Create Database connection---------------------------------
 # connection = db.connect('DRIVER={SQL Server};'
 #                         'SERVER=137.116.139.217;'
 #                         'DATABASE=ARCHIVESKF;'
 #                         'UID=sa;PWD=erp@123')
 #
+#
 # query = """ select top 1000 * from OEsalesDetails """
+#
 # data = pd.read_sql_query(query, connection)
+# data.to_csv('sales_data.csv', index=False)
+# print('Data Saved')
+
+# print(type(data))
 
 # # Get data from csv files ----------------------------------
-data = pd.read_csv('./Data/sales_data.csv')
-# print(data.head())
+data = pd.read_csv('D:/Python Code/Best-on-Python/sales_data.csv')
+
+# print(data.head(4))
 # print(type(data))
 
 
-# # Creating DataFrame----------------------------------------
+# # Creating DataFrame ----------------------------------------
 # df = pd.DataFrame({'Yes': [10, 20], 'No': [4, 30]})
 # print(df)
 
@@ -33,13 +40,15 @@ data = pd.read_csv('./Data/sales_data.csv')
 
 # # Working with dataset ---------------------------------------
 # # Print all columns
-print(data.columns)
+# print(data.columns)
 
 # # Print Dataset shape
 # print(data.shape)
 
 # Select fixed columns
 # print(data.AUDTORG)
+# print(data.AUDTORG.tolist())
+
 # print(data['AUDTORG'])
 
 # Select Multiple columns
@@ -49,14 +58,14 @@ print(data.columns)
 # print(data.AUDTORG.iloc[0])
 
 # Select fixed column and row
-# print(data.AUDTORG[10])
+# print(data.AUDTORG[100])
 
 # Select by index  [:1] indicates total number of rows with all columns
 # print(data.iloc[:2])
 
 # # Condition
-# rng = data.loc[(data.AUDTORG == 'RNGSKF')]
-# print(rng)
+rng = data.loc[(data.AUDTORG == 'RNGSKF')]
+print(rng)
 
 
 # # To Write csv Files
