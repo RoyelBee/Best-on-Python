8ba0078886bb30c55715af3b39036bcaa893e316
Royel <royelsoft@gmail.com>
2020-06-29 23:21:42 +0600

    Bar chart basics

--- matplotlib_basic.py
+++ matplotlib_basic.py
@@ -11,6 +11,8 @@ from matplotlib import pyplot as plt
 # plt.show()
 
 # # For multiple lines ---------------------
+
+
 x = ['A', 'B', 'C', 'D', 'E']
 y = [24, 40, 30, 50, 45]
 z = [10, 20, 28, 14, 35]
@@ -24,6 +26,16 @@ ax.legend(labels=('Math', 'English'), loc='best')  # legend placed at lower righ
 plt.xlabel("Name",  color='black', fontsize=14, fontweight='bold')
 plt.ylabel("Marks", color='black', fontsize=14, fontweight='bold')
 plt.title('Line chart', fontweight='bold', color='#3e0a75',  fontsize=18)
-# plt.show()
+
+def autolabel(line1):
+    # attach some text labels
+    for line in line1:
+        height = int(line.get_height())
+        ax.text(line.get_x() + line.get_width(), height,
+                thousand_k_convert(height), ha='center', va='bottom', fontsize=12, fontweight='bold')
+
+
+autolabel(line1)
+plt.show()
 # plt.tight_layout()
-plt.savefig('linechart.png')
+# plt.savefig('linechart.png')
